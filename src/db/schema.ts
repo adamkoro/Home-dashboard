af4ecfad61c6c97720afceeb34f5418b43081bf5
adamkoro <adam.koronics@gmail.com>
2024-05-04 12:35:30 +0200

    feat: users table

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,16 @@
 import { int, mysqlTable, varchar, text } from 'drizzle-orm/mysql-core';
 
+export const users = mysqlTable('users', {
+	id: int('id').primaryKey(),
+	githubId: varchar('github_id', { length: 255 }).unique(),
+	username: varchar('username', { length: 255 }),
+	email: varchar('email', { length: 255 }).unique(),
+	accessToken: text('access_token')
+});
+
 export const links = mysqlTable('links', {
 	id: int("id").primaryKey(),
 	name: varchar("name", { length: 256 }),
-	url: text("url")
+	url: text("url"),
+	userId: int('user_id').references(() => users.id)
 });
