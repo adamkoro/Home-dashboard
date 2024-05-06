55f4e9e88a6717d99a14159bd6f152f4222ec0f1
adamkoro <adam.koronics@gmail.com>
2024-05-06 21:40:55 +0200

    feat: home page

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import AuthCheck from '$lib/components/AuthCheck.svelte';
+	import { signOut } from 'firebase/auth';
+	import { auth } from '$lib/firebase';
+</script>
+
+<AuthCheck>
+	<h1>Home page</h1>
+	<h3>Welcome</h3>
+	<button on:click={() => signOut(auth)}> Sign out</button>
+</AuthCheck>
