bf7c37eb5e9708d041912e33a7864fc09696ba88
adamkoro <adam.koronics@gmail.com>
2024-05-06 21:39:50 +0200

    feat: route to the best place

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { auth } from '$lib/firebase';
+	import { goto } from '$app/navigation';
+
+	onMount(() => {
+		const unsubscribe = auth.onAuthStateChanged(user => {
+			if (user) {
+				goto("/home");
+			} else {
+				goto("/login");
+			}
+		});
+
+		return () => {
+			unsubscribe();
+		};
+	});
+</script>
+
+
+
