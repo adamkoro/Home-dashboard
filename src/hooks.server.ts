f1bbb0e0e989a0d4645f583741649e17ff4fd87c
adamkoro <adam.koronics@gmail.com>
2024-05-07 18:16:16 +0200

    feat: hooks to check is a user is authenticated or not

---
+++ src/hooks.server.ts
@@ -0,0 +1,17 @@
+import { adminAuth } from '$lib/server/firebaseAdmin';
+import { type Handle, redirect } from '@sveltejs/kit';
+
+export const handle = (async ({event, resolve}) => {
+	const sessionCookie = event.cookies.get('__session');
+	try {
+		const decodedClaims = await adminAuth.verifySessionCookie(sessionCookie!);
+		event.locals.email = decodedClaims.email;
+	} catch (e) {
+		event.locals.email = undefined;
+		if (event.url.pathname !== '/login' && !event.url.pathname.startsWith('/api/')) {
+			redirect(301, "/login")
+		}
+
+	}
+	return resolve(event);
+}) satisfies Handle;
