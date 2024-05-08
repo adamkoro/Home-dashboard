976344faa7167d7bfce857a3997de4cdd5882ace
adamkoro <adam.koronics@gmail.com>
2024-05-08 11:36:01 +0200

    feat: fix some security apis and DELETE method

--- src/routes/api/links/+server.ts
+++ src/routes/api/links/+server.ts
@@ -35,15 +35,17 @@ export async function POST(event: RequestEvent): Promise<Response> {
 	try {
 		const requestData: link = await event.request.json();
 
-		if (!requestData.name || !requestData.url) {
+		if (!requestData.email || requestData.email !== event.locals.email) {
+			return new Response(JSON.stringify({ type: "error", message: "Must be an authenticated user" }), { status: 401 });
+		}
+		if (!requestData.name || !requestData.url ||!requestData.email) {
 			return new Response(JSON.stringify({ type: "error", message: "Missing required fields" }), { status: 400 });
 		}
 
-		
         const result = await db.insert(links).values({
             name: requestData.name,
             url: requestData.url,
-			email: event.locals.email
+			email: requestData.email
 		});
 
         if (result) {
@@ -55,4 +57,31 @@ export async function POST(event: RequestEvent): Promise<Response> {
         console.error('Error handling POST request:', error);
         return new Response(JSON.stringify({ type: "error", message: "Internal Server Error" }), { status: 500 });
     }
+}
+
+export async function DELETE(event: RequestEvent): Promise<Response> {
+	try {
+		if (!event.locals.email) {
+			return new Response(JSON.stringify({ type: "error", message: "Invalid token" }), { status: 401 });
+		}
+
+		let url = new URL(event.request.url);
+		let linkId = url.searchParams.get("id");
+
+		if (linkId === null) {
+			return new Response(JSON.stringify({ type: "error", message: "Link ID is required" }), { status: 400 });
+		}
+
+		const result = await db.delete(links).where(eq(links.id,parseInt(linkId)));
+
+		if (result == null) {
+			return new Response(JSON.stringify({ type: "error", message: "No link found with the provided ID" }), { status: 404 });
+		}
+		
+		return new Response(JSON.stringify({ type: "success", message: "Link deleted successfully" }), { status: 200 });
+
+	} catch (error) {
+		console.error('Error verifying Firebase ID token:', error);
+		return new Response(JSON.stringify({ type: "error", message: "Internal Server Error" }), { status: 500 });
+	}
 }
