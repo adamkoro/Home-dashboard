bff66f01a1d99921de9f30861814cd1cf1f1081c
adamkoro <adam.koronics@gmail.com>
2024-05-08 10:40:03 +0200

    fix: autoincrement pk and unique removed

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,8 @@
 import { int, mysqlTable, varchar, text } from 'drizzle-orm/mysql-core';
 
 export const links = mysqlTable('links', {
-	id: int("id").primaryKey(),
+	id: int("id").primaryKey().autoincrement(),
 	name: varchar("name", { length: 256 }),
 	url: text("url"),
-	email: varchar('email', { length: 256 }).unique()
+	email: varchar('email', { length: 256 })
 });
