c581a4457e2d251a49d9ce5c9f65abb800b50512
adamkoro <adam.koronics@gmail.com>
2024-05-08 10:40:26 +0200

    fix: env from dotenv

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,17 @@
+import * as dotenv from 'dotenv';
 import 'dotenv/config';
 import type { Config } from 'drizzle-kit';
-import { DATABASE_HOST, DATABASE_PORT, DATABASE_USER, DATABASE_PASSWORD, DATABASE_NAME } from './src/db/db';
 
+dotenv.config()
 export default {
 	schema: './src/db/schema.ts',
 	out: './drizzle',
 	driver: 'mysql2',
 	dbCredentials: {
-		host: DATABASE_HOST,
-		port: DATABASE_PORT,
-		user: DATABASE_USER,
-		password: DATABASE_PASSWORD,
-		database: DATABASE_NAME,
+		host: process.env.SECRET_DB_HOST || "127.0.0.1",
+		port: parseInt(process.env.SECRET_DB_PORT || "3306"),
+		user: process.env.SECRET_DB_USERNAME || "dev",
+		password: process.env.SECRET_DB_PASSWORD || "dev",
+		database: process.env.SECRET_DB_NAME || "home_dashboard",
 	},
 } satisfies Config;
--- src/db/db.ts
+++ src/db/db.ts
@@ -1,17 +1,18 @@
 import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2';
-import { SECRET_DB_HOST, SECRET_DB_PORT, SECRET_DB_USERNAME, SECRET_DB_PASSWORD, SECRET_DB_NAME } from '$env/static/private';
+import * as dotenv from 'dotenv';
 
-if (!SECRET_DB_HOST || !SECRET_DB_PORT || !SECRET_DB_USERNAME || !SECRET_DB_PASSWORD || !SECRET_DB_NAME) {
+dotenv.config();
+if (!process.env.SECRET_DB_HOST || !process.env.SECRET_DB_PORT || !process.env.SECRET_DB_USERNAME || !process.env.SECRET_DB_PASSWORD || !process.env.SECRET_DB_NAME) {
 	throw new Error('Database configuration is missing, check the env variables');
 }
 
 export const connection = mysql.createConnection({
-	host: SECRET_DB_HOST,
-	user: SECRET_DB_USERNAME,
-	password: SECRET_DB_PASSWORD,
-	database: SECRET_DB_NAME ,
-	port: parseInt(SECRET_DB_PORT)
+	host: process.env.SECRET_DB_HOST,
+	user: process.env.SECRET_DB_USERNAME,
+	password: process.env.SECRET_DB_PASSWORD,
+	database: process.env.SECRET_DB_NAME ,
+	port: parseInt(process.env.SECRET_DB_PORT)
 });
 
 export const db = drizzle(connection);
